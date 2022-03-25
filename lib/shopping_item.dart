a3fa2eda109644f56449f3f590f5aa3e6a76966e
Lukas Steinmann <lukas.steinmann@gmx.de>
2022-03-25 21:10:00 +0100

    Added persistent data using json

---
+++ lib/shopping_item.dart
@@ -0,0 +1,25 @@
+///Model class to represent items to shop
+/// @author Lukas Steinmann <lukas.steinmann@gmx.de>
+class ShoppingItem {
+  bool? bought;
+  final String name;
+  final int amount;
+
+  ShoppingItem(this.bought, this.name, this.amount);
+
+  factory ShoppingItem.fromJson(Map<String, dynamic> data)
+  {
+    final bought = data['bought'] as bool?;
+    final name = data['name'] as String;
+    final amount = data['amount'] as int;
+    return ShoppingItem(bought, name, amount);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'bought': bought,
+      'name': name,
+      'amount': amount,
+    };
+  }
+}
